1a01ce6c09e51a5ba6be08554871c994964c578f
David Eisner <david.eisner@oriel.oxon.org>
2014-02-03 15:57:19 +0000

    Alternative navigation pattern

--- pageslider.js
+++ pageslider.js
@@ -10,15 +10,23 @@ define(function (require) {
             currentPage,
             stateHistory = [];
 
-        this.back = function () {
-            location.hash = stateHistory[stateHistory.length - 2];
+        self.back = function () {
+            var
+                prev = stateHistory.length - 2,
+                ret = prev >= 0;
+            if (ret)
+                self.slidePage($("[data-PageSlider='" + stateHistory[prev] + "']"));
+            return ret;
         };
 
         // Use this function if you want PageSlider to automatically determine the sliding direction based on the state history
         self.slidePage = function (page, after) {
 
             var l = stateHistory.length,
-                state = window.location.hash;
+                state = page.attr('data-PageSlider');
+
+            if (!state)
+                page.attr('data-PageSlider', state = Math.random() + "");
 
             if (l === 0) {
                 stateHistory.push(state);
@@ -51,9 +59,13 @@ define(function (require) {
 
             // Position the page at the starting position of the animation
             page.attr("class", "page " + from);
+            page.removeClass("finished");
 
             currentPage.one('webkitTransitionEnd', function (e) {
-                $(e.target).remove();
+                if (from == "page-left")
+                    $(e.target).remove();
+                else
+                    $(e.target).addClass("finished");
             });
 
             // Force reflow. More information here: http://www.phpied.com/rendering-repaint-reflowrelayout-restyle/
