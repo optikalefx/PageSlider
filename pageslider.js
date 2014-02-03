66eb07aceff19f3efbacef862c1e84fa2c1192ea
David Eisner <david.eisner@oriel.oxon.org>
2014-02-03 15:52:49 +0000

    AMD from directory-backbone-topcoat-require sample app

--- pageslider.js
+++ pageslider.js
@@ -1,60 +1,66 @@
-/* Notes:
- * - History management is currently done using window.location.hash.  This could easily be changed to use Push State instead.
- * - jQuery dependency for now. This could also be easily removed.
- */
+define(function (require) {
 
-function PageSlider(container) {
+    "use strict";
 
-    var container = container,
-        currentPage,
-        stateHistory = [];
+    var $ = require('jquery');
 
-    // Use this function if you want PageSlider to automatically determine the sliding direction based on the state history
-    this.slidePage = function(page) {
+    return function PageSlider(container) {
 
-        var l = stateHistory.length,
-            state = window.location.hash;
+        var currentPage,
+            stateHistory = [];
 
-        if (l === 0) {
-            stateHistory.push(state);
-            this.slidePageFrom(page);
-            return;
-        }
-        if (state === stateHistory[l-2]) {
-            stateHistory.pop();
-            this.slidePageFrom(page, 'left');
-        } else {
-            stateHistory.push(state);
-            this.slidePageFrom(page, 'right');
-        }
+        this.back = function () {
+            location.hash = stateHistory[stateHistory.length - 2];
+        };
 
-    }
+        // Use this function if you want PageSlider to automatically determine the sliding direction based on the state history
+        this.slidePage = function (page) {
 
-    // Use this function directly if you want to control the sliding direction outside PageSlider
-    this.slidePageFrom = function(page, from) {
+            var l = stateHistory.length,
+                state = window.location.hash;
 
-        container.append(page);
+            if (l === 0) {
+                stateHistory.push(state);
+                this.slidePageFrom(page);
+                return;
+            }
+            if (state === stateHistory[l - 2]) {
+                stateHistory.pop();
+                this.slidePageFrom(page, 'page-left');
+            } else {
+                stateHistory.push(state);
+                this.slidePageFrom(page, 'page-right');
+            }
 
-        if (!currentPage || !from) {
-            page.attr("class", "page center");
+        };
+
+        // Use this function directly if you want to control the sliding direction outside PageSlider
+        this.slidePageFrom = function (page, from) {
+
+            container.append(page);
+
+            if (!currentPage || !from) {
+                page.attr("class", "page page-center");
+                currentPage = page;
+                return;
+            }
+
+            // Position the page at the starting position of the animation
+            page.attr("class", "page " + from);
+
+            currentPage.one('webkitTransitionEnd', function (e) {
+                $(e.target).remove();
+            });
+
+            // Force reflow. More information here: http://www.phpied.com/rendering-repaint-reflowrelayout-restyle/
+            container[0].offsetWidth;
+
+            // Position the new page and the current page at the ending position of their animation with a transition class indicating the duration of the animation
+            page.attr("class", "page transition page-center");
+            currentPage.attr("class", "page transition " + (from === "page-left" ? "page-right" : "page-left"));
             currentPage = page;
-            return;
-        }
+        };
 
-        // Position the page at the starting position of the animation
-        page.attr("class", "page " + from);
+    };
 
-        currentPage.one('webkitTransitionEnd', function(e) {
-            $(e.target).remove();
-        });
-
-        // Force reflow. More information here: http://www.phpied.com/rendering-repaint-reflowrelayout-restyle/
-        container[0].offsetWidth;
-
-        // Position the new page and the current page at the ending position of their animation with a transition class indicating the duration of the animation
-        page.attr("class", "page transition center");
-        currentPage.attr("class", "page transition " + (from === "left" ? "right" : "left"));
-        currentPage = page;
-    }
-
-}
+});
